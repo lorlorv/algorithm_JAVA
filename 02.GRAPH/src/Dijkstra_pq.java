e198459181922819a939b50b1ccc6bdbcaecdcef
이은서 <leuns36@naver.com>
2021-08-24 21:39:03 +0900

    210824 : Dijkstra (PQ) 코드 수정 + 주석 추가

--- 02.GRAPH/src/Dijkstra_pq.java
+++ 02.GRAPH/src/Dijkstra_pq.java
@@ -18,23 +18,6 @@ class graph_pq{
 	private int n; //노드들의 수 
 	private int maps[][]; //노드들의 가중치 저장
 	
-	public graph_pq(int n) {
-		this.n = n;
-		maps = new int[n + 1][n + 1];
-		
-		//인접행렬 초기화
-		for(int i=1; i<=n; ++i){
-			for(int j=1; j<=n; ++j){
-				maps[i][j] = Integer.MAX_VALUE; 
-			} 			
-		}		
-	}
-	
-	public void input(int i, int j, int w) {
-		maps[i][j] = w;
-		maps[j][i] = w;
-	}
-	
 	class Node implements Comparable<Node>{
 		private int weight;
 		private int index; 
@@ -51,6 +34,25 @@ class graph_pq{
 		
 	}
 	
+	//생성자
+	public graph_pq(int n) { //초기화 역할
+		this.n = n;
+		maps = new int[n + 1][n + 1];
+		
+		//인접행렬 초기화
+		for(int i=1; i<=n; ++i){
+			for(int j=1; j<=n; ++j){
+				maps[i][j] = Integer.MAX_VALUE; 
+			} 			
+		}		
+	}
+	
+	public void input(int i, int j, int w) {
+		maps[i][j] = w;
+		maps[j][i] = w;
+	}
+	
+		
 	public void dijkstra_pq(int v) {
 		PriorityQueue<Node> pq = new PriorityQueue<>();
 		int distance[] = new int[n + 1];
@@ -62,31 +64,29 @@ class graph_pq{
 		}
 		//우선순위 큐에 start 정점과 index를 삽입
 		pq.add(new Node(v, 1));
-		distance[v] = 0;
+		distance[v] = 0; //시작점이니까 distance는 0
 		check[v] = true;
 		
 		//연결노드 distance 갱신
 		for(int i = 1; i <= n; i++) {
 			if(!check[i] && maps[v][i] != Integer.MAX_VALUE) {
 				distance[i] = maps[v][i];
-				pq.add(new Node(maps[v][i], i)); //우선순위 큐에 weight ← 정점의 가중치, i는 정점의 index
+				pq.add(new Node(distance[i], i)); //우선순위 큐에 weight ← 정점의 가중치, i는 정점의 index
 			}
 		}
 		
 		while(!pq.isEmpty()) {
-			int min = Integer.MAX_VALUE;
 			int index;
 			
 			//노드 최소값 꺼내기 
 			Node node = pq.poll(); //가장 최소값 
-			min = node.weight;
 			index = node.index;
 			
 			check[index] = true;
 			//distance 갱신
 			for(int i = 1; i <= n; i++) {
 				if(!check[i] && maps[index][i] != Integer.MAX_VALUE) {
-					if(distance[i] > distance[index] + maps[index][i]) {
+					if(distance[i] > distance[index] + maps[index][i]) { //현재 노드를 거쳐가는 것이 더 짧으면
 						distance[i] = distance[index] + maps[index][i];
 						pq.add(new Node(distance[i], i));
 					}
