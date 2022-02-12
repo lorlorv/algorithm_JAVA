6914582927bd78054b89be5589d4737cf82a9bf9
lorlorv <dev.eunseo@gmail.com>
2022-02-12 21:09:19 +0900

    프로그래머스 : x만큼 간격이 있는 n개의 숫자(java파일만 업로드)

--- programmers_lv1/src/programmers_lv1/lv1_1.java
+++ programmers_lv1/src/programmers_lv1/lv1_1.java
@@ -18,7 +18,7 @@ public class lv1_1 {
 		//testcase 13,14 fail
 		//혹시 x가 0이라면? ->문제 없음.,,,
 		//혹시 n이 0이라면? -> 문제 없음 
-		//해결! -> 질문창을 보고 해결함 매개변수 int x 를 long x로 바꾼
+		//해결! -> 질문창을 보고 해결함 매개변수 int x 를 long x로 바꾼다 
 	    long[] answer = {};
 	    answer = new long[n];
 	    for(int i = 0; i < n; i++){
