8cd4c136806fb09a2834c719e9ca0af6108e68c0
lorlorv <dev.eunseo@gmail.com>
2022-02-13 14:41:37 +0900

    프로그래머스 : 행렬의 덧셈

---
+++ programmers_lv1/src/programmers_lv1/lv1_2.java
@@ -0,0 +1,20 @@
+package programmers_lv1;
+
+class lv1_2 {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = {};
+        answer = new int[arr1.length][arr1[0].length];
+        
+        //        j j
+        //i = 0 | 1 2    3 4  | 1 2  3 4 
+        //i = 1 | 2 3    5 6  |
+        
+        for(int i = 0; i < arr1.length; i++){
+            for(int j = 0; j < arr1[0].length; j++){
+                answer[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+        
+        return answer;
+    }
+}
